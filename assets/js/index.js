74813b965410589b1cb6d038e2231c554a2cab87
Alisson Gonçalves <30551666+AlissonRoGon@users.noreply.github.com>
2018-05-25 00:10:57 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,14 +19,33 @@
 // })
 
 
-$('.navbar a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
+// $('.nav a[href^="#"]').on('click', function(e) {
+// 	e.preventDefault();
+	
 			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
-});
+// 	$('html, body').animate({ 
+// 		scrollTop: targetOffset - 100
+// 	}, 500);
+// });
 
+// $(".navbar div").on('click', function(){
+// 	var destiny = "." + $(this).data('destiny');
+// 	var destinyPosition = $(destiny).position().top;
+// 	$('html, body').stop().animate({
+// 		scrollTop: destinyPosition
+// 	}, 2000)
+// });
 
+// $(".but").on('click', function(){
+// 	console.log("eba");
+// });
+
+// $('.navbar a[href^="#"]').on('click', function(e) {
+//   e.preventDefault();
+//   var id = $(this).attr('href'),
+//   targetOffset = $(id).offset().top;
+    
+//   $('html, body').animate({ 
+//     scrollTop: targetOffset - 100
+//   }, 500);
+// });
